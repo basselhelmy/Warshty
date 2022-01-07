31efff95ccd9756da340d21d1142da1acb9459a9
SeifAldinYasserEl-Tfakshi <gc1376319@gmail.com>
2022-01-07 13:16:18 +0200

    project2

---
+++ lib/places/utils.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class Utils {
+  static Future openLink({
+     String url,
+  }) =>
+      _launchUrl(url);
+
+
+
+  static Future openPhoneCall({ String phoneNumber}) async {
+    final url = 'tel:$phoneNumber';
+
+    await _launchUrl(url);
+  }
+
+  static Future openSMS({ String phoneNumber}) async {
+    final url = 'sms:$phoneNumber';
+
+    await _launchUrl(url);
+  }
+
+  static Future _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+}
